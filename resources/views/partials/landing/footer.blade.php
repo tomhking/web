5c1c65f83a20a73fa764fc4eb707ead0764e188c
dovile <dovile.tarutyte@hostinger.com>
2017-09-27 16:39:16 +0300

    Menu and subscribe fixes

--- resources/views/partials/landing/footer.blade.php
+++ resources/views/partials/landing/footer.blade.php
@@ -15,6 +15,7 @@
                             <!--<li><a href="#token-sale-terms" data-toggle="collapse" data-target=".navbar-collapse.in">@lang('navigation.ico')</a></li>-->
                             <!--<li><a href="#faqs" data-toggle="collapse" data-target=".navbar-collapse.in">@lang('navigation.faq')</a></li>-->
                             <!--<li><a href="https://medium.com/@bitdegree" target="_blank">@lang('navigation.blog')</a></li>-->
+                            <li><a href="{{ route_lang('home') }}#section-five" data-toggle="collapse" data-target=".navbar-collapse.in">@lang('navigation.mvp')</a></li>
                             <li><a href="/bitdegree-ico-one-pager.pdf" class="navbar-cta" target="_blank">@lang('navigation.one-pager')</a></li>
                             <li><a href="{{ asset('files/white-paper.pdf') }}" class="navbar-cta" target="_blank">@lang('navigation.white-paper')</a></li>
                         </ul>
