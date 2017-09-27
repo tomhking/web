10334653dcf4288ce13ba86c719effb503059aab
Martynas Andriuškevičius <martynas7272@gmail.com>
2017-09-27 09:23:53 +0300

    Add multi-language support (#5)
    * Add multi-language support
    
    * Initialize language switcher
    
    * Fix form action path
    
    * Include ru fonts
    
    * Edit css
    
    * Use Gulp to compile stylesheets
    
    * Edit css
    
    * Show current language
    
    * Better font
    
    * Fix translations
    
    * Fix language selection for mobile
    
    * Fix some css
    
    * Fix some css
    
    * Fix some css
    
    * Fix one class
    
    * Add advisor

---
+++ app/Http/Middleware/LanguageMiddleware.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class LanguageMiddleware
+{
+    private $fallbackLanguage = 'en';
+    private $supportedLanguages = [
+        'en' => 'EN',
+        'ch' => 'CH',
+        'ru' => 'RU',
+    ];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $language = $request->segment(1);
+
+        if (!isset($this->supportedLanguages[$language])) {
+            abort(404);
+        }
+
+        view()->share([
+            'languages' => $this->supportedLanguages,
+            'currentLanguage' => $language,
+            'defaultLanguage' => $this->fallbackLanguage,
+        ]);
+
+        /**
+         * @var \Illuminate\Translation\Translator $translator
+         */
+        $translator = app()->make('translator');
+        $translator->setLocale($language);
+        $translator->setFallback($this->fallbackLanguage);
+
+        return $next($request);
+    }
+}
