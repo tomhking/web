a612964b9b2535e140c28b693c8e60ad6087154d
dovile <dovile.tarutyte@hostinger.com>
2017-10-02 17:38:18 +0300

    Update telegram link

--- resources/views/partials/contact-icons.blade.php
+++ resources/views/partials/contact-icons.blade.php
@@ -5,6 +5,6 @@
     <a class="contact-icon" href="https://www.reddit.com/r/bitdegree" target="_blank"><i class="fa fa-reddit-alien" aria-hidden="true"></i></a>
     <a class="contact-icon" href="https://www.facebook.com/bitdegree.org/" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
     <a class="contact-icon" href="https://blog.bitdegree.org/" rel="nofollow" target="_blank"><i class="fa fa-medium" aria-hidden="true"></i></a>
-    <a class="contact-icon" href="https://t.me/bitdegree" rel="nofollow" target="_blank"><i class="fa fa-telegram" aria-hidden="true"></i></a>
+    <a class="contact-icon" href="https://t.me/joinchat/GIngsQrKak9hN8h4xwN2Kg" rel="nofollow" target="_blank"><i class="fa fa-telegram" aria-hidden="true"></i></a>
     <a class="contact-icon" href="https://bitcointalk.org/index.php?topic=2214321" rel="nofollow" target="_blank"><i class="fa fa-btc" aria-hidden="true"></i></a>
 </div>
--- resources/views/partials/landing/header.blade.php
+++ resources/views/partials/landing/header.blade.php
@@ -80,7 +80,7 @@
                             <div class="contact-icons">
                                 <a class="contact-icon" href="https://twitter.com/bitdegree_org" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
                                 <a class="contact-icon" href="https://slack.bitdegree.org" target="_blank"><i class="fa fa-slack" aria-hidden="true"></i></a>
-                                <a class="contact-icon" href="https://t.me/bitdegree" rel="nofollow" target="_blank"><i class="fa fa-telegram" aria-hidden="true"></i></a>
+                                <a class="contact-icon" href="https://t.me/joinchat/GIngsQrKak9hN8h4xwN2Kg" rel="nofollow" target="_blank"><i class="fa fa-telegram" aria-hidden="true"></i></a>
                                 <a class="contact-icon" href="https://bitcointalk.org/index.php?topic=2214321" rel="nofollow" target="_blank"><i class="fa fa-btc" aria-hidden="true"></i></a>
 
                             </div>
