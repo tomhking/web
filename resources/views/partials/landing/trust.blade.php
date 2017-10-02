1293ec1b445524a5434e1d0031ef45ae241b79a0
dovile <dovile.tarutyte@hostinger.com>
2017-10-02 10:55:40 +0300

    Add more trust logos

--- resources/views/partials/landing/trust.blade.php
+++ resources/views/partials/landing/trust.blade.php
@@ -9,20 +9,55 @@
 
         <div class="row logos">
             <div class="col-md-10 col-md-offset-1">
-                <div class="col-xs-6 col-sm-4 text-center">
-                    <a href="http://www.huffingtonpost.com/entry/the-future-of-degree_us_59c165a7e4b082fd4205ba50" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/huffpost.png') }}" alt="As seen on Huffpost"></a>
+                <div class="row">
+                    <div class="col-xs-6 col-sm-3 text-center">
+                        <a href="http://www.huffingtonpost.com/entry/the-future-of-degree_us_59c165a7e4b082fd4205ba50" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/huffpost.png') }}" alt="As seen on Huffpost"></a>
+                    </div>
+                    <div class="col-xs-6 col-sm-3 text-center">
+                        <a href="https://channels.theinnovationenterprise.com/articles/blockchain" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/innovation-enterprise.png') }}" alt="As seen on Innovation Enterprise"></a>
+                    </div>
+                    <div class="col-xs-6 col-sm-3 text-center">
+                        <a href="https://onmogul.com/stories/revolutionizing-education-4-ways-the-blockchain-will-change-the-way-we-learn" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/onmogul.png') }}" alt="As seen on Mogul"></a>
+                    </div>
+                    <div class="col-xs-6 col-sm-3 text-center">
+                        <a href="https://e27.co/blockchain-tech-will-disrupt-education-ways-expect-20170927/" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/e27.png') }}" alt="As seen on e27"></a>
+                    </div>
                 </div>
-                <div class="col-xs-6 col-sm-4 text-center">
-                    <a href="https://channels.theinnovationenterprise.com/articles/blockchain" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/innovation-enterprise.png') }}" alt="As seen on Innovation Enterprise"></a>
+                <div class="row">
+                    <div class="col-xs-6 col-sm-3 text-center">
+                        <a href="http://www.finanzen.ch/nachrichten/aktien/BitDegree-Challenges-Coursera-and-HackerRanks-Phenomenon-by-Gearing-Up-With-Blockchain-1002908591" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/finanzen.png') }}" alt="As seen on Finanzen.ch"></a>
+                    </div>
+                    <div class="col-xs-6 col-sm-3 text-center">
+                        <a href="https://sg.news.yahoo.com/blockchain-tech-disrupt-education-more-ways-expect-003022725.html" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/yahoo.png') }}" alt="As seen on Yahoo News"></a>
+                    </div>
+                    <div class="col-xs-6 col-sm-3 text-center">
+                        <a href="https://thenextweb.com/guests/employer-demanded-skills-school-grad-skills-huge-gap/#.tnw_VDEPyYzB" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/tnw.png') }}" alt="As seen on The Next Web"></a>
+                    </div>
+                    <div class="col-xs-6 col-sm-3 text-center">
+                        <a href="http://markets.businessinsider.com/news/stocks/BitDegree-Challenges-Coursera-and-HackerRank-s-Phenomenon-by-Gearing-Up-With-Blockchain-1002908591" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/markets-insider.png') }}" alt="As seen on Markets Insider"></a>
+                    </div>
                 </div>
-                <div class="col-xs-6 col-sm-4 text-center">
-                    <a href="https://onmogul.com/stories/revolutionizing-education-4-ways-the-blockchain-will-change-the-way-we-learn" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/onmogul.png') }}" alt="As seen on Mogul"></a>
+                <div class="row">
+                    <div class="col-xs-6 col-sm-3 text-center">
+                        <a href="http://bitcoinprbuzz.com/bitdegree-challenges-coursera-hackerranks-phenomenon-gearing-blockchain/" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/bitcoinprbuzz.png') }}" alt="As seen on BitcoinPRbuzz"></a>
+                    </div>
+                    <div class="col-xs-6 col-sm-3 text-center">
+                        <a href="https://www.thestreet.com/story/14325627/1/bitdegree-challenges-coursera-and-hackerranks-phenomenon-by-gearing-up-with-blockchain.html" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/thestreet.png') }}" alt="As seen on The Street"></a>
+                    </div>
+                    <div class="col-xs-6 col-sm-3 text-center">
+                        <a href="https://seekingalpha.com/pr/16955692-bitdegree-challenges-coursera-hackerranks-phenomenon-gearing-blockchain" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/seeking-alpha.png') }}" alt="As seen on Seeking Alpha"></a>
+                    </div>
+                    <div class="col-xs-6 col-sm-3 text-center">
+                        <a href="https://www.bizjournals.com/prnewswire/press_releases/2017/09/30/NY04107" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/business-journals.png') }}" alt="As seen on Business Journals"></a>
+                    </div>
                 </div>
-                <div class="col-xs-6 col-sm-4 col-sm-offset-2 text-center">
-                    <a href="https://e27.co/blockchain-tech-will-disrupt-education-ways-expect-20170927/" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/e27.png') }}" alt="As seen on e27"></a>
-                </div>
-                <div class="col-xs-6 col-sm-4 text-center">
-                    <a href="https://sg.news.yahoo.com/blockchain-tech-disrupt-education-more-ways-expect-003022725.html" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/yahoo.png') }}" alt="As seen on Yahoo News"></a>
+                <div class="row">
+                    <div class="col-xs-6 col-sm-3 text-center">
+                        <a href="http://www.marketwatch.com/story/bitdegree-challenges-coursera-and-hackerranks-phenomenon-by-gearing-up-with-blockchain-2017-09-30" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/market-watch.png') }}" alt="As seen on MarketWatch"></a>
+                    </div>
+                    <div class="col-xs-6 col-sm-3 text-center">
+                        <a href="https://cointelegraph.com/press-releases/ea-co-founder-jeff-burton-to-advise-bitdegree-on-a-course-to-revolutionize-education-with-blockchain" rel="nofollow" target="_blank"><img src="{{ asset('trust-logos/cointelegraph2.png') }}" alt="As seen on Cointelegraph"></a>
+                    </div>
                 </div>
             </div>
         </div>
