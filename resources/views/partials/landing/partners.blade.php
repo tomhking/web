aecd688f0042e7a77fc328d5aeb168f0b620346b
dovile <dovile.tarutyte@hostinger.com>
2017-10-11 09:24:41 +0300

    Hide bitcoin suisse

--- resources/views/partials/landing/partners.blade.php
+++ resources/views/partials/landing/partners.blade.php
@@ -11,7 +11,7 @@
 
             <div class="row cards">
 
-                <div class="col-xs-12 col-sm-4">
+                <div class="col-xs-12 col-sm-6">
                     <div class="card left">
                         <div class="partner-logo">
                             <a href="https://preply.com/" rel="nofollow" target="_blank"><img src="{{ asset('partners/preply.png') }}" alt="Our partners Preply"></a>
@@ -23,10 +23,10 @@
                     </div>
                 </div>
 
-                <div class="col-xs-12 col-sm-4">
+                <div class="col-xs-12 col-sm-6">
                     <div class="card">
                         <div class="partner-logo">
-                             <a href="https://preply.com/" rel="nofollow" target="_blank"><img src="{{ asset('partners/tokenlot.png') }}" alt="Our partners Tokenlot"></a>
+                             <a href="https://www.tokenlot.com/" rel="nofollow" target="_blank"><img src="{{ asset('partners/tokenlot.png') }}" alt="Our partners Tokenlot"></a>
                         </div>
                         <div class="partner-desc">
                             <p>TokenLot in an ICO superstore which offers access to more ICO’s than any other service. Their service is intuitive and easy to use.</p>
@@ -35,16 +35,18 @@
                     </div>
                 </div>
 
+            <!--
                 <div class="col-xs-12 col-sm-4">
                     <div class="card right">
                         <div class="partner-logo">
-                            <a href="https://preply.com/" rel="nofollow" target="_blank"><img src="{{ asset('partners/bitcoin-suisse.png') }}" alt="Our partners Bitcoin Suisse"></a>
+                            <a href="https://www.bitcoinsuisse.ch/" rel="nofollow" target="_blank"><img src="{{ asset('partners/bitcoin-suisse.png') }}" alt="Our partners Bitcoin Suisse"></a>
                         </div>
                         <div class="partner-desc">
                             <p>Bitcoin Suisse AG is a regulated crypto financial broker, asset manager and service provider based in Zug, Switzerland. The company operates a network of bitcoin ATMs in most major Swiss towns, performs exchange- and treasury services, and trades with and on behalf of a number of the major companies and actors in the virtual currency and asset markets.</p>
                         </div>
                     </div>
                 </div>
+             -->
 
             </div>
 
