e690e816ebf719f6861eb9c178de1b2619a7e713
dovile <dovile.tarutyte@hostinger.com>
2017-10-16 10:25:07 +0300

    Edit telegram link

--- resources/views/partials/chat.blade.php
+++ resources/views/partials/chat.blade.php
@@ -1,11 +1,11 @@
 <div class="chat-container">
         <div class="chat-icon">
-            <a href="https://t.me/joinchat/GIngsQrKak9hN8h4xwN2Kg" rel="nofollow" target="_blank">
+            <a href="https://t.me/bitdegree" rel="nofollow" target="_blank">
                 <i class="fa fa-telegram" aria-hidden="true"></i>
             </a>
         </div>
     <div class="chat-text">
-        <a href="https://t.me/joinchat/GIngsQrKak9hN8h4xwN2Kg" rel="nofollow" target="_blank">
+        <a href="https://t.me/bitdegree" rel="nofollow" target="_blank">
             <h4>Let's chat</h4>
         </a>
     </div>
--- resources/views/partials/landing/header.blade.php
+++ resources/views/partials/landing/header.blade.php
@@ -88,7 +88,7 @@
                                 @endif
                             @endif
                             <div class="contact-icons buttons">
-                                <a class="contact-icon" href="https://t.me/joinchat/GIngsQrKak9hN8h4xwN2Kg" rel="nofollow" target="_blank"><img src="{{ asset('telegram-logo.png') }}" alt="Telegram"></a>
+                                <a class="contact-icon" href="https://t.me/bitdegree" rel="nofollow" target="_blank"><img src="{{ asset('telegram-logo.png') }}" alt="Telegram"></a>
                                 <a class="contact-icon" href="https://twitter.com/bitdegree_org" rel="nofollow" target="_blank"><img src="{{ asset('twitter-logo.png') }}" alt="Twitter"></a>
                             </div>
                         </div>
