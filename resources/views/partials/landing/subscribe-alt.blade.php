3676c0cb82a26e17e8712cf70c2aaa1d5a613ea3
dovile <dovile.tarutyte@hostinger.com>
2017-10-25 17:03:38 +0300

    Fix languages layout

--- resources/views/partials/landing/subscribe-alt.blade.php
+++ resources/views/partials/landing/subscribe-alt.blade.php
@@ -4,12 +4,16 @@
             <div class="col-xs-12 col-md-8 col-md-push-2 text-center">
                 <h2 class="title">@lang('subscribe.title_alt')</h2>
             </div>
-            <div class="contact">
-                <form action="https://xyz.us16.list-manage.com/subscribe/post?u=528cc9372b916077746636344&amp;id=f79db67249" method="post">
-                    <input class="suscribe-input" name="EMAIL" type="email" placeholder="@lang('subscribe.email_placeholder')" required>
-                    <input type="submit" class="submit" value="@lang('subscribe.button')" name="subscribe">
-                </form>
-                @include('partials.contact-icons')
+            <div class="row find-out-more">
+                <div class="col-xs-12 text-center">
+                    <div class="contact">
+                        <form action="https://xyz.us16.list-manage.com/subscribe/post?u=528cc9372b916077746636344&amp;id=f79db67249" method="post">
+                            <input class="suscribe-input" name="EMAIL" type="email" placeholder="@lang('subscribe.email_placeholder')" required>
+                            <input type="submit" class="submit" value="@lang('subscribe.button')" name="subscribe">
+                        </form>
+                        @include('partials.contact-icons')
+                    </div>
+                </div>
             </div>
         </div>
     </div>
