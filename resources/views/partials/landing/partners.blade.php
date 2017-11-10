e79482f4e17a4c22856ca57895bd4e701f1416dc
dovile <dovile.tarutyte@hostinger.com>
2017-11-10 17:25:27 +0200

    Add preply

--- resources/views/partials/landing/partners.blade.php
+++ resources/views/partials/landing/partners.blade.php
@@ -6,25 +6,29 @@
             </div>
         </div>
 
-        <div class="partners col-md-8 col-md-offset-2">
-                <div class="col-xs-12 col-xs-4 col-sm-4 text-center">
+        <div class="col-xs-12 col-xs-6 col-sm-3 text-center">
+            <div class="partner-logo">
+                <a href="https://preply.com" rel="nofollow" target="_blank"><img src="{{ asset('partners/preply.png') }}" alt="Our partners Preply"></a>
+            </div>
+        </div>
+
+                <div class="col-xs-12 col-xs-6 col-sm-3 text-center">
                         <div class="partner-logo">
                              <a href="https://www.tokenlot.com/" rel="nofollow" target="_blank"><img src="{{ asset('partners/tokenlot.png') }}" alt="Our partners Tokenlot"></a>
                         </div>
                 </div>
 
-            <div class="col-xs-12 col-xs-4 col-sm-4 text-center">
+            <div class="col-xs-12 col-xs-6 col-sm-3 text-center">
                 <div class="partner-logo">
                     <a href="https://bitcoinfoundation.org/" rel="nofollow" target="_blank"><img src="{{ asset('partners/bitcoin-foundation-small.png') }}" alt="Our partners Tokenlot"></a>
                 </div>
             </div>
 
-            <div class="col-xs-12 col-xs-4 col-sm-4 text-center">
+            <div class="col-xs-12 col-xs-6 col-sm-3 text-center">
                 <div class="partner-logo">
                     <a href="https://entethalliance.org/" rel="nofollow" target="_blank"><img src="{{ asset('partners/ethereum-alliance-small.png') }}" alt="Our partners Tokenlot"></a>
                 </div>
             </div>
 
-        </div>
     </div>
 </div>
