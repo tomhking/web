971a3905b978827622373a5b64026a545edf0fa2
dovile <dovile.tarutyte@hostinger.com>
2017-11-17 11:47:26 +0200

    FIX mvp

--- resources/views/partials/mvp-available.blade.php
+++ resources/views/partials/mvp-available.blade.php
@@ -1,24 +1,16 @@
 <div class="main mvp-available">
-    <div class="container top">
+    <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 main-content">
-                <div class="col-xs-12 col-sm-12 col-md-12  mvp-wrapper">
-                    <div class="content front-content" id="content">
-                        <div class="container-fluid communicate">
-                            <div class="row">
-                                <div class="col-xs-12 col-md-10 col-md-push-1 text-center">
-                                    <h6><b>MVP</b> AVAILABLE:</h6>
-                                    <h1>Solidity Smart Contract Development</h1>
-                                    <p class="subtitle"><b>Solidity</b> is a programming language for writing smart contracts which run on Ethereum Virtual Machine on Blockchain. It is a contract-oriented, high-level language whose syntax is similar to that of JavaScript. It's designed to target the Ethereum Virtual Machine. Master this course to become a Smart Contract Developer.</p>
-                                </div>
-                                <div class="mvp-cta text-center">
-                                    <a href="https://solidity.bitdegree.org/" class="cta-btn">START LEARNING</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            <div class="mvp-wrapper">
+                <div class="col-xs-12 col-md-10 col-md-push-1 text-center">
+                    <h6><b>MVP</b> AVAILABLE:</h6>
+                    <h1>Solidity Smart Contract Development</h1>
+                    <p class="subtitle">The Solidity course material for programming smart contracts is ready. Start learning now and gain skills needed to become a Smart Contract Developer.</p>
+                </div>
+                <div class="col-xs-12 col-md-10 col-md-push-1 mvp-cta text-center">
+                    <a href="https://solidity.bitdegree.org/" class="cta-btn">START LEARNING</a>
                 </div>
             </div>
-        </div>
+         </div>
     </div>
 </div>
