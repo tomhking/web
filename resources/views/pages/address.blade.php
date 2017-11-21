6b6fb83de92bf1a5f567dc5332e524730fb7edb4
dovile <dovile.tarutyte@hostinger.com>
2017-11-21 09:48:42 +0200

    Fix design

--- resources/views/pages/address.blade.php
+++ resources/views/pages/address.blade.php
@@ -1,66 +1,87 @@
-@extends('layouts.landing', ['navBarOnly' => true, 'bodyClass' => 'degree-list'])
+@extends('layouts.landing', ['navBarOnly' => true, 'bodyClass' => 'login-page get-tokens', 'hideFooter' => true])
 
 @section('content')
 
-    @if($participant->isProfileFull())
-        <h1>Get Tokens</h1>
-        <div class="form-group">
-            <label for="input-address">Send ETH to this address:</label>
-            <input type="text" class="form-control" readonly value="{{ $icoAddress }}">
-            <p class="text-right copy">
-                <a>Copy Address</a>
-            </p>
-        </div>
-        <div class="well">
-            <p>Please make sure to have a valid ERC20 compatible Ethereum address to receive your tokens. Do not use any exchange address!</p>
-            <p>Recommended Gas Limit: 200 000. Payments without data or gas limit fields are rejeced.</p>
-            <p><strong>Do NOT send ETH from an exchange.</strong> Use MyEtherWallet, MetaMask, Mist, Parity or other compatible wallets.</p>
-        </div>
-    @else
-        <div class="row">
-            <div class="col-md-12">
-                <h1>Personal Details</h1>
-            </div>
-        </div>
+    <div class="main">
+        <div class="container">
+            <a href="{{ route_lang('home') }}" class="login-logo">
+                <img class="logo" src="{{ asset('bitdegree-logo.png') }}" alt="BitDegree">
+            </a>
 
-        <form action="{{ route_lang('participant-profile') }}" method="post">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="input-first-name">First Name</label>
-                        <input type="text" class="form-control" id="input-first-name" name="first_name">
+            <div class="content main">
+                @if($participant->isProfileFull())
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <h1>Get tokens</h1>
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="input-country">Country</label>
-                        <select name="country" id="input-country" class="form-control">
-                            <option value="">- please select -</option>
-                            @foreach($countries as $code => $country)
-                                <option value="{{ $code }}" {{ in_array($code, $blacklistedCountries) ? "disabled" : "" }} {{ $currentCountry == $code && !in_array($code, $blacklistedCountries) ? "selected" : "" }}>{{ $country }}</option>
-                            @endforeach
-                        </select>
+                    <div class="row">
+                        <div class="col-xs-12 col-md-10 col-md-offset-1 eth-address">
+                            <div class="form-group">
+                                <label for="input-address">Send ETH to this address:</label>
+                                <input type="text" class="form-control" readonly value="{{ $icoAddress }}">
+                                <p class="text-right copy">
+                                    <a>Copy Address</a>
+                                </p>
+                            </div>
+                            <div class="well">
+                                <p>Please make sure to have a valid ERC20 compatible Ethereum address to receive your tokens. Do not use any exchange address!</p>
+                                <p>Recommended Gas Limit: 200 000. Payments without data or gas limit fields are rejeced.</p>
+                                <p><strong>Do NOT send ETH from an exchange.</strong> Use MyEtherWallet, MetaMask, Mist, Parity or other compatible wallets.</p>
+                            </div>
+                        </div>
+                    </div>
+                @else
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <h1>Personal Details</h1>
+                        </div>
+                    </div>
+
+                <div class="row">
+                    <div class="col-xs-12 col-md-10 col-md-offset-1 personal-details">
+                        <form action="{{ route_lang('participant-profile') }}" method="post">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="input-first-name">First Name</label>
+                                    <input type="text" class="form-control" id="input-first-name" name="first_name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="input-country">Country</label>
+                                    <select name="country" id="input-country" class="form-control">
+                                        <option value="">- please select -</option>
+                                        @foreach($countries as $code => $country)
+                                            <option value="{{ $code }}" {{ in_array($code, $blacklistedCountries) ? "disabled" : "" }} {{ $currentCountry == $code && !in_array($code, $blacklistedCountries) ? "selected" : "" }}>{{ $country }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="input-last-name">Last Name</label>
+                                    <input type="text" class="form-control" id="input-last-name" name="last_name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="input-birthday">Date of Birth</label>
+                                    <input type="date" class="form-control" id="input-birthday" name="birthday" max="{{ \Carbon\Carbon::today()->subYears(16)->toDateString() }}" min="{{ \Carbon\Carbon::today()->subYears(100)->toDateString() }}">
+                                    <span class="text-danger validation-error"></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <p class="text-center agreement">By proceeding you agree to our <a href="#">Terms of Service</a></p>
+                                <div class="text-center cta"><button type="submit" class="btn btn-primary">Get Tokens</button></div>
+                            </div>
+                        </div>
+                    </form>
                     </div>
                 </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="input-last-name">Last Name</label>
-                        <input type="text" class="form-control" id="input-last-name" name="last_name">
-                    </div>
-                    <div class="form-group">
-                        <label for="input-birthday">Date of Birth</label>
-                        <input type="date" class="form-control" id="input-birthday" name="birthday" max="{{ \Carbon\Carbon::today()->subYears(16)->toDateString() }}" min="{{ \Carbon\Carbon::today()->subYears(100)->toDateString() }}">
-                        <span class="text-danger validation-error"></span>
-                    </div>
-                </div>
+                @endif
             </div>
-
-            <div class="row">
-                <div class="col-md-12">
-                    <p class="text-center">By proceeding you agree to our <a href="#">Terms of Service</a></p>
-                    <div class="text-center"><button type="submit" class="btn btn-primary">Get Tokens</button></div>
-                </div>
-            </div>
-        </form>
-    @endif
-
+        </div>
+    </div>
 
 @endsection
--- resources/views/partials/email-modal.blade.php
+++ resources/views/partials/email-modal.blade.php
@@ -4,13 +4,12 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title">Enter email to get <strong>BitDegree Tokens</strong></h4>
+                    <h4 class="modal-title">Enter email to get<br> <strong>BitDegree Tokens</strong></h4>
                 </div>
                 <div class="modal-body" id="modal-agreements">
 
                         <div class="form-group">
-                            <label for="input-email">Your Email</label>
-                            <input type="email" class="form-control" value="" name="email" placeholder="you@example.com" required>
+                            <input type="email" class="form-control" value="" name="email" placeholder="Your email" required>
                         </div>
                 </div>
                 <div class="modal-footer">
