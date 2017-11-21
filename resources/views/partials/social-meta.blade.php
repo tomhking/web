1f673b379a4825f10f89f6cf405baf6e819d0b47
Martynas <martynas.andriuskevicius@hostinger.com>
2017-11-21 10:49:05 +0200

    Add Facebook OG meta tags and optimize images

---
+++ resources/views/partials/social-meta.blade.php
@@ -0,0 +1,21 @@
+<link rel="shortcut icon" href="{{ base('fav_icon.ico') }}" type="image/x-icon">
+<link rel="apple-touch-icon" sizes="57x57" href="{{ asset('app-icons/apple-icon-57x57.png') }}">
+<link rel="apple-touch-icon" sizes="60x60" href="{{ asset('app-icons/apple-icon-60x60.png') }}">
+<link rel="apple-touch-icon" sizes="72x72" href="{{ asset('app-icons/apple-icon-72x72.png') }}">
+<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('app-icons/apple-icon-76x76.png') }}">
+<link rel="apple-touch-icon" sizes="114x114" href="{{ asset('app-icons/apple-icon-114x114.png') }}">
+<link rel="apple-touch-icon" sizes="120x120" href="{{ asset('app-icons/apple-icon-120x120.png') }}">
+<link rel="apple-touch-icon" sizes="144x144" href="{{ asset('app-icons/apple-icon-144x144.png') }}">
+<link rel="apple-touch-icon" sizes="152x152" href="{{ asset('app-icons/apple-icon-152x152.png') }}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('app-icons/apple-icon-180x180.png') }}">
+<link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('app-icons/android-icon-192x192.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ base('fav-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="96x96" href="{{ base('fav-96x96.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ base('fav-16x16.png') }}">
+<link rel="manifest" href="{{ asset('app-icons/manifest.json') }}">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="{{ asset('app-icons/ms-icon-144x144.png') }}">
+<meta name="theme-color" content="#ffffff">
+<meta property="og:url" content="{{ app('request')->url() }}" />
+<meta property="og:type" content="website" />
+<meta property="og:image" content="{{ asset('app-icons/facebook.jpg') }}" />
