2deeb881f458f7753cb926634b4a710b54ec5588
dovile <dovile.tarutyte@hostinger.com>
2017-11-22 09:43:46 +0200

    Fix twitter feed

--- resources/views/partials/landing/twitter-feed.blade.php
+++ resources/views/partials/landing/twitter-feed.blade.php
@@ -1,8 +1,13 @@
-<div class="twitter-feed main">
+<div class="twitter-feed main ">
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <a class="twitter-timeline" data-width="800" data-height="600" href="https://twitter.com/bitdegree_org?ref_src=twsrc%5Etfw">Tweets by bitdegree_org</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            <div class="col-md-6 col-md-offset-3">
+                <a class="twitter-timeline"
+                   data-width="585"
+                   data-height="600"
+                   href="https://twitter.com/bitdegree_org?ref_src=twsrc%5Etfw">
+                </a>
+                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
             </div>
         </div>
     </div>
