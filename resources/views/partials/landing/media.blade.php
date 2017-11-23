110117dcda6417b87a8ead84e4d1193f211ee66c
dovile <dovile.tarutyte@hostinger.com>
2017-11-23 16:43:21 +0200

    Fix article layout

--- resources/views/partials/landing/media.blade.php
+++ resources/views/partials/landing/media.blade.php
@@ -10,28 +10,28 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="row row-articles">
-                    <div class="col-xs-12 col-sm-3">
+                    <div class="col-xs-6 col-sm-6 col-md-3">
                         <a class="article-thumbnail" href="https://cointelegraph.com/news/ec-releases-report-on-possible-blockchain-applications-in-education" rel="nofollow" target="_blank">
                             <img src="{{ asset('media-posts/blockchain-in-education.jpg') }}" alt="EC: Blockchain Applications in Education">
                             <h4>EC: Blockchain Applications in Education</h4>
                             <p class="article-date"><span>Nov 17, 2017</span></p>
                         </a>
                     </div>
-                    <div class="col-xs-12 col-sm-3">
+                    <div class="col-xs-6 col-sm-6 col-md-3">
                         <a class="article-thumbnail" href="http://coinwisdom.org/26117-bitdegree-teams-up-with-zcoin-to-develop-cryptography-course/" rel="nofollow" target="_blank">
                             <img src="{{ asset('media-posts/zcoin-partnership.jpg') }}" alt="BitDegree teams up with Zcoin to develop cryptography course">
                             <h4>BitDegree teams up with Zcoin to develop cryptography course</h4>
                             <p class="article-date"><span>Nov 12, 2017</span></p>
                         </a>
                     </div>
-                    <div class="col-xs-12 col-sm-3">
+                    <div class="col-xs-6 col-sm-6 col-md-3">
                         <a class="article-thumbnail" href="https://blog.bitdegree.org/steve-jobsschool-founder-joins-bitdegree-as-advisor-2167bf04312c" rel="nofollow" target="_blank">
                             <img src="{{ asset('media-posts/maurice-joins-bitdegree.jpg') }}" alt="Steve JobsSchool Founder Joins">
                             <h4>Steve JobsSchool Founder Joins</h4>
                             <p class="article-date"><span>Nov 2, 2017</span></p>
                         </a>
                     </div>
-                    <div class="col-xs-12 col-sm-3">
+                    <div class="col-xs-6 col-sm-6 col-md-3">
                         <a class="article-thumbnail" href="https://steemit.com/blockchain/@bitdegree/press-release-former-coursera-senior-manager-joins-bitdegree-as-advisor" rel="nofollow" target="_blank">
                             <img src="{{ asset('media-posts/roberto-joins-bitdegree.jpg') }}" alt="Former Coursera Senior Product Manager has joined Bitdegree">
                             <h4>Former Coursera Senior Product Manager has joined Bitdegree</h4>
@@ -41,28 +41,28 @@
                 </div>
                 <div class="row row-articles">
 
-                    <div class="col-xs-12 col-sm-3">
+                    <div class="col-xs-6 col-sm-6 col-md-3">
                         <a class="article-thumbnail" href="https://www.youtube.com/watch?v=zbLAl9HRfAY" rel="nofollow" target="_blank">
                             <img src="{{ asset('media-posts/jeff-joins-bitdegree.jpg') }}" alt="Jeff Burton - EA co-founder talks about BitDegree">
                             <h4>Jeff Burton - EA co-founder talks about BitDegree</h4>
                             <p class="article-date"><span>Oct 11, 2017</span></p>
                         </a>
                     </div>
-                    <div class="col-xs-12 col-sm-3">
+                    <div class="col-xs-6 col-sm-6 col-md-3">
                         <a class="article-thumbnail" href="https://www.ted.com/talks/bill_gates_teachers_need_real_feedback/transcript?language=en" rel="nofollow" target="_blank">
                             <img src="{{ asset('media-posts/bill-gates-ted.jpg') }}" alt="Bill Gates At TED Talks Education">
                             <h4>Bill Gates At TED Talks Education</h4>
                             <p class="article-date"><span>May 2013</span></p>
                         </a>
                     </div>
-                    <div class="col-xs-12 col-sm-3">
+                    <div class="col-xs-6 col-sm-6 col-md-3">
                         <a class="article-thumbnail" href="https://www.youtube.com/watch?v=Ue_h6CDngjM" rel="nofollow" target="_blank">
                             <img src="{{ asset('media-posts/interview-with-roberto.jpg') }}" alt="Roberto Santana about BitDegree">
                             <h4>Roberto Santana about BitDegree</h4>
                             <p class="article-date"><span>Nov 13, 2017</span></p>
                         </a>
                     </div>
-                    <div class="col-xs-12 col-sm-3">
+                    <div class="col-xs-6 col-sm-6 col-md-3">
                         <a class="article-thumbnail" href="https://blog.bitdegree.org/guaranteed-value-return-for-bitdegree-token-holders-backed-by-hostinger-services-ac5a675b9528" rel="nofollow" target="_blank">
                             <img src="{{ asset('media-posts/backed-by.jpg') }}" alt="Token Backed by Hostinger services">
                             <h4>Token Value Backed by Hostinger services</h4>
@@ -72,28 +72,28 @@
             </div>
                 <div class="row row-articles">
 
-                    <div class="col-xs-12 col-sm-3">
+                    <div class="col-xs-6 col-sm-6 col-md-3">
                         <a class="article-thumbnail" href="https://blog.bitdegree.org/q-a-with-crypto-economy-advisor-oleg-belousov-73fe19c34076" rel="nofollow" target="_blank">
                             <img src="{{ asset('media-posts/qa-with-oleg.jpg') }}" alt="Jeff Burton - EA co-founder talks about BitDegree">
                             <h4>Q&A with Crypto-Economy advisor</h4>
                             <p class="article-date"><span>Nov 22, 2017</span></p>
                         </a>
                     </div>
-                    <div class="col-xs-12 col-sm-3">
+                    <div class="col-xs-6 col-sm-6 col-md-3">
                         <a class="article-thumbnail" href="https://blog.bitdegree.org/bitdegree-takes-further-security-measures-to-ensure-token-holder-safety-ahead-of-token-sale-7426b1596cec" rel="nofollow" target="_blank">
                             <img src="{{ asset('media-posts/bdg-security.jpg') }}" alt="Bill Gates At TED Talks Education">
                             <h4>More security to protect BitDegree token</h4>
                             <p class="article-date"><span>Nov 17, 2017</span></p>
                         </a>
                     </div>
-                    <div class="col-xs-12 col-sm-3">
+                    <div class="col-xs-6 col-sm-6 col-md-3">
                         <a class="article-thumbnail" href="https://twitter.com/bitdegree_org/status/930385214005764096" rel="nofollow" target="_blank">
                             <img src="{{ asset('media-posts/purity-declaration.jpg') }}" alt="Roberto Santana about BitDegree">
                             <h4>Bitdegree signed the Purity declaration</h4>
                             <p class="article-date"><span>Nov 14, 2017</span></p>
                         </a>
                     </div>
-                    <div class="col-xs-12 col-sm-3">
+                    <div class="col-xs-6 col-sm-6 col-md-3">
                         <a class="article-thumbnail" href="https://medium.com/the-mission/focus-on-learning-and-creating-rather-than-being-entertained-and-distracted-e6573de1bc84" rel="nofollow" target="_blank">
                             <img src="{{ asset('media-posts/focus-on-learning.jpg') }}" alt="Token Backed by Hostinger services">
                             <h4>Focus On Learning</h4>
