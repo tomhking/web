0236972d6759b485fc59057899631f958adde7fa
dovile <dovile.tarutyte@hostinger.com>
2017-11-27 14:51:00 +0200

    Buttons

--- resources/views/pages/crowdsaleaddress.blade.php
+++ resources/views/pages/crowdsaleaddress.blade.php
@@ -12,13 +12,10 @@
                     </a>
                 </div>
             </div>
-            <div class="col-md-5 col-md-push-4 align-right">
+            <div class="col-md-2 col-md-push-7 align-right">
                 <div class="user-menu">
                     <ul>
-                        <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i> Referral</a></li>
-                        <li><a href="#"> <img class="token" src="{{ asset('token-img.png') }}" alt="BitDegree Token"> Aidrop: <b>1BDG</b></a></li>
-                        <li><a href="#"><img class="ethereum" src="{{ asset('ethereum-icon.png') }}" alt="Ethereum"> Add your Wallet address</a></li>
-                        <li><a href="#"><i class="fa fa-user" aria-hidden="true"></i> Account</a></li>
+                        <li><a href="#"><i class="fa fa-user" aria-hidden="true"></i> Change pasword</a></li>
                         <li><a href="#"><i class="fa fa-sign-out" aria-hidden="true"></i> Log Out</a></li>
                     </ul>
                 </div>
@@ -28,17 +25,24 @@
 
     <div class="container-fluid">
         <div class="col-sm-3 col-md-2 hidden-xs-down sidebar">
-            <div class="main">
-                <h4>What's next?</h4>
-                <ul class="sidebar-nav">
-                    <li class="step-done"><a href="#">Step 1</a></li>
-                    <li class="step-active"><a href="#">Step 2</a></li>
-                    <li class="step-other"><a href="#">Other</a></li>
+            <h4>What's next?</h4>
+            <ul class="sidebar-nav">
+                <li class="step-done"><a href="#">Step 1</a></li>
+                <li class="step-active"><a href="#">Step 2</a></li>
+                <li class="step-other"><a href="#">Other</a></li>
+            </ul>
+        </div>
+
+        <div class="col-sm-9 offset-sm-3 col-md-10 col-md-offset-2 pt-3 ">
+            <div class="dashboard-navigation">
+                <ul class="nav nav-tabs">
+                    <li class="active"><a href="#">Crowdsale</a></li>
+                    <li><a href="#"><i class="fa fa-question-circle" aria-hidden="true"></i> How to participate</a></li>
+                    <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i> Earn more </a></li>
+                    <li><a href="#"><img class="token" src="{{ asset('token-img.png') }}" alt="BitDegree Token"> My Airdrops</a></li>
                 </ul>
             </div>
-        </div>
-        <div class="col-sm-9 offset-sm-3 col-md-10 col-md-offset-2 pt-3 container-main">
-            <div class="main">
+            <div class="main container-main">
                 <div class="container">
                     <div class="row">
                         <div class="col-xs-12 col-md-10 col-md-offset-1 eth-address">
@@ -61,7 +65,7 @@
                                     <p>1 ETH = 10,000 BDG*</p>
 
                                     <h4>Gas Limit</h4>
-                                    <p>20,000</p>
+                                    <p>200,000</p>
 
                                 </div>
 
@@ -79,18 +83,27 @@
                     <div class="row">
                         <div class="col-xs-12 col-md-10 col-md-offset-1">
                                 <div class="well">
-                                <p>Please make sure to have a valid ERC20 compatible Ethereum address to receive your tokens. <b><span class="red">Do not use any exchange address!</span></b></p>
-                                <p><b>Recommended Gas Limit: 200 000.</b> Payments without data or gas limit fields are rejeced.</p>
-                                <p><strong>Do NOT send ETH from an exchange.</strong> Use MyEtherWallet, MetaMask, Mist, Parity or other compatible wallets.</p>
+                                <p>Please make sure to have a valid ERC20 compatible Ethereum address to receive your tokens. <b><span class="red">Do not use any exchange address!</span></b> <strong>Do NOT send ETH from an exchange.</strong> Use MyEtherWallet, MetaMask, Mist, Parity or other compatible wallets.</p>
                             </div>
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="col-xs-12 col-md-10 col-md-offset-1 text-center">
-                            <a class="how-to-participate" href="">How to participate?</a>
+                        <div class="col-xs-12 col-md-10 col-md-offset-1 buttons">
+                            <div class="content dashboard-buttons">
+                                <div class="left text-left">
+                                    <a class="how-to-participate" href="">How to participate?</a>
+                                </div>
+                                <div class="right text-right">
+                                    <div class="cta">
+                                        <button type="submit" class="btn btn-primary button-done">DONE</button></div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
+
+
                 </div>
             </div>
         </div>
