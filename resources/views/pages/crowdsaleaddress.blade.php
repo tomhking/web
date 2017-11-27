20bc315b81e21c47109de2276ee994199e344dbe
dovile <dovile.tarutyte@hostinger.com>
2017-11-27 15:38:06 +0200

    add links

--- resources/views/pages/crowdsaleaddress.blade.php
+++ resources/views/pages/crowdsaleaddress.blade.php
@@ -38,8 +38,8 @@
                 <ul class="nav nav-tabs">
                     <li class="active"><a href="#">Crowdsale</a></li>
                     <li><a href="#"><i class="fa fa-question-circle" aria-hidden="true"></i> How to participate</a></li>
-                    <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i> Earn more </a></li>
-                    <li><a href="#"><img class="token" src="{{ asset_rev('token-img.png') }}" alt="BitDegree Token"> My Airdrops</a></li>
+                    <li><a href="{{ route('affiliate') }}"><i class="fa fa-star" aria-hidden="true"></i> Earn more </a></li>
+                    <li><a href="{{ route('user') }}"><img class="token" src="{{ asset_rev('token-img.png') }}" alt="BitDegree Token"> My Airdrops</a></li>
                 </ul>
             </div>
             <div class="main container-main">
