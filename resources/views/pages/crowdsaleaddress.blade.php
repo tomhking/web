88ddcb4b567e4a8fb18b263f8de87ec64ef4ba81
dovile <dovile.tarutyte@hostinger.com>
2017-11-27 14:08:53 +0200

    Some layout edits

--- resources/views/pages/crowdsaleaddress.blade.php
+++ resources/views/pages/crowdsaleaddress.blade.php
@@ -41,24 +41,54 @@
             <div class="main">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-12 text-center">
-                            <h1>Send ETH to this address and receive BDG tokens <b>NOW!</b></h1>
-                        </div>
-                    </div>
-                    <div class="row">
                         <div class="col-xs-12 col-md-10 col-md-offset-1 eth-address">
+                            <h1 class="text-center">Send ETH to this address and receive BDG tokens <b>NOW!</b></h1>
                             <div class="form-group">
                                 <input type="text" class="form-control" readonly value="" onclick="" id="ico-address">
-
                                 <p class="text-right" id="copy-address">
                                     <a class="btn">Copy Address</a>
                                 </p>
                             </div>
-                            <div class="well">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-xs-12 col-md-10 col-md-offset-1">
+                            <div class="crowdsale-info-table">
+
+                                <div class="col-xs-6 col-sm-6 col-md-6">
+                                    <h4>Exchange rate</h4>
+                                    <p>1 ETH = 10,000 BDG*</p>
+
+                                    <h4>Gas Limit</h4>
+                                    <p>20,000</p>
+
+                                </div>
+
+                                <div class="col-xs-6 col-sm-6 col-md-6">
+                                    <h4>Week 1 Bonus</h4>
+                                    <p>15%</p>
+
+                                    <h4>Week 1 Bonus ends in:</h4>
+                                    <p>24:15:20</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-xs-12 col-md-10 col-md-offset-1">
+                                <div class="well">
                                 <p>Please make sure to have a valid ERC20 compatible Ethereum address to receive your tokens. <b><span class="red">Do not use any exchange address!</span></b></p>
                                 <p><b>Recommended Gas Limit: 200 000.</b> Payments without data or gas limit fields are rejeced.</p>
                                 <p><strong>Do NOT send ETH from an exchange.</strong> Use MyEtherWallet, MetaMask, Mist, Parity or other compatible wallets.</p>
                             </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-xs-12 col-md-10 col-md-offset-1 text-center">
+                            <a class="how-to-participate" href="">How to participate?</a>
                         </div>
                     </div>
                 </div>
