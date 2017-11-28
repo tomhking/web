eceae35e22cd80fcd55041aa4900ef2e2fc5980b
dovile <dovile.tarutyte@hostinger.com>
2017-11-28 14:27:41 +0200

    instructions wip

--- resources/views/pages/participate.blade.php
+++ resources/views/pages/participate.blade.php
@@ -3,21 +3,65 @@
 @section('content')
 
     <div class="container-fluid">
-        <div class="col-sm-3 col-md-2 hidden-xs-down sidebar">
-            <h4>What's next?</h4>
-            <ul class="sidebar-nav">
-                <li class="step-done"><a href="#">Step 1</a></li>
-                <li class="step-active"><a href="{{ route('address') }}">Step 2</a></li>
-                <li class="step-other"><a href="#"><span>Other</span></a></li>
-            </ul>
-        </div>
+        @include('partials.instructions-menu')
 
         <div class="col-sm-12  col-md-10 col-md-offset-2 pt-3 ">
 
             <div class="main container-main">
                 <div class="container">
                     <div class="row">
-                        <h1 class="text-center">How to participate?</h1>
+                        <h1 class="text-center">How to create an ether wallet</h1>
+                        <p class="subtitle">First of all you need to have an Ether wallet. Creating an ether wallet is a must in order to participate in the crowdsale and receive your tokens.</p>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-xs-12 col-md-10 col-md-offset-1">
+                            <div id="accordion" role="tablist">
+                                <div class="card">
+                                    <div class="card-header" role="tab" id="headingOne">
+                                        <h5 class="mb-0">
+                                            <div data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                                MyEtherWallet [RECOMMENDED]
+                                            </div>
+                                        </h5>
+                                    </div>
+
+                                    <div id="collapseOne" class="collapse show" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
+                                        <div class="card-body">
+                                            <p><b>Step 1:</b> Go to <a href="https://www.myetherwallet.com/" target="_blank" rel="nofollow">https://www.myetherwallet.com/</a> and make sure the URL is correct and MYETHERWALLET LLC [US] certificate is there.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <div class="card-header" role="tab" id="headingTwo">
+                                        <h5 class="mb-0">
+                                            <a class="collapsed" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                                Collapsible Group Item #2
+                                            </a>
+                                        </h5>
+                                    </div>
+                                    <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion">
+                                        <div class="card-body">
+                                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <div class="card-header" role="tab" id="headingThree">
+                                        <h5 class="mb-0">
+                                            <a class="collapsed" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                                Collapsible Group Item #3
+                                            </a>
+                                        </h5>
+                                    </div>
+                                    <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
+                                        <div class="card-body">
+                                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
                     <div class="row">
@@ -29,8 +73,6 @@
                             </div>
                         </div>
                     </div>
-
-
                 </div>
             </div>
         </div>
---
+++ resources/views/partials/instructions-menu.blade.php
@@ -0,0 +1,9 @@
+<div class="col-sm-3 col-md-2 hidden-xs-down sidebar">
+    <h4>What's next?</h4>
+    <ul class="sidebar-nav">
+        <li class="instructions"><a href="#">How to create an ether wallet</a></li>
+        <li class="instructions"><a href="{{ route('address') }}">How to get Ethereum and transfer it to your wallet</a></li>
+        <li class="instructions"><a href="#">Alternative ways to get tokens</a></li>
+        <li class="instructions"><a href="#">FAQ</a></li>
+    </ul>
+</div>
