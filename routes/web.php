210f9773e6a17076bfb40962cb3be2da64cd540a
Martynas <martynas.andriuskevicius@hostinger.com>
2017-11-30 15:42:28 +0200

    Add a separate page for identity verification

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,8 @@ Route::group(['prefix' => $locale = language_prefix()], function() use ($locale)
             Route::get('/address', 'UserController@address')->name('address');
             Route::get('/profile', 'UserController@showDetails')->name('details');
             Route::post('/profile', 'UserController@details');
+            Route::get('/identification', 'UserController@showIdentification')->name('identification');
+            Route::post('/identification', 'UserController@identification');
             Route::get('/password', 'UserController@showPassword')->name('password');
             Route::post('/password', 'UserController@password');
             Route::get('/participate', 'UserController@participate')->name('participate');
