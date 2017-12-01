3fba58a07768ec11d23367215b99d5ca389dd14d
Martynas <martynas.andriuskevicius@hostinger.com>
2017-12-01 19:52:25 +0200

    Add link to medium tutorial

--- resources/views/partials/instructions-menu.blade.php
+++ resources/views/partials/instructions-menu.blade.php
@@ -3,6 +3,7 @@
     <ul class="sidebar-nav">
         <li class="instructions"><a href="{{ route('participate') }}">@lang('ico-instructions.article-get-wallet')</a></li>
         <li class="instructions"><a href="{{ route('participate2') }}">@lang('ico-instructions.article-get-eth')</a></li>
+        <li class="instructions"><a href="https://blog.bitdegree.org/how-to-buy-bdg-tokens-with-myetherwallet-810275ebe882" target="_blank">How to get BDG tokens with ETH</a></li>
         <li class="instructions"><a href="{{ route('participate3') }}">@lang('ico-instructions.article-get-tokens')</a></li>
         <li class="instructions"><a href="{{ route('participatefaq') }}">@lang('ico-instructions.article-faq')</a></li>
     </ul>
