c5fb29c58a1a5756ea2d1ce269d6b177127ddb64
dovile <dovile.tarutyte@hostinger.com>
2017-12-01 13:07:46 +0200

    Edit top info for the crowdsale

--- resources/views/partials/top-information.blade.php
+++ resources/views/partials/top-information.blade.php
@@ -1,7 +1,14 @@
 <div class="top-info-container">
     <div class="top-text-container">
-        <a href="#subscribe-to-get-bonus">
-            <p>@lang('home.ico_top_information')</p>
-        </a>
+        @if(config('ico.start')->isFuture())
+            <a href="#subscribe-to-get-bonus">
+                <p>@lang('home.ico_top_information')</p>
+            </a>
+        @else
+            <a href="#top">
+                <p>@lang('home.ico_top_information_2')</p>
+            </a>
+        @endif
+
     </div>
 </div>
