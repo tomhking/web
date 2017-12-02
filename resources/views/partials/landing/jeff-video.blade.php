d90476faf28267f23a9ecddb307427881562cf8e
Martynas <martynas.andriuskevicius@hostinger.com>
2017-12-02 11:58:41 +0200

    Change homepage video

--- resources/views/partials/landing/jeff-video.blade.php
+++ resources/views/partials/landing/jeff-video.blade.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 description">
             <div class="image-container text-center">
-                <iframe width="820" height="456" src="https://www.youtube.com/embed/zbLAl9HRfAY?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                <iframe width="820" height="456" src="https://www.youtube.com/embed/R0pKfprwSAI?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
             </div>
         </div>
     </div>
