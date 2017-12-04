c6bdf59b84af5fde807b3f98dac1b19d3013e0de
Martynas <martynas.andriuskevicius@hostinger.com>
2017-12-04 15:26:58 +0200

    Update TR translations

---
+++ resources/lang/tr/affiliate.php
@@ -0,0 +1,24 @@
+<?php 
+
+return array (
+  'referral_link_label' => 'KİŞİSEL REFERANS BAĞLANTINIZ:',
+  'referrals_count_label ' => 'TOKEN PAYINIZ (REFERANS KULLANICILARDAN):',
+  'headline' => 'BitDegree Referans Token Dağıtımı',
+  'subtitle' => 'Daha fazla ÜCRETSİZ token mi istiyorsunuz? 200 000 BDG Token havuzundan payışınızı şimdi alın!',
+  'how' => 'Nasıl çalışır?',
+  'how-descr-1' => 'Sayfanın başında eşsiz referans bağlantınızı bulacaksınız. ÇOK SAYIDA token kazanmak için en kolay yöntemin bağlantınızı arkadaşlarınızla, topluluklarla ve diğer herkesle paylaşmak olduğunu unutmayın!',
+  'how-descr-2' => 'Ayrıca, web sitelerinize yerleştirmek ve daha fazla kişiyi yönlendirmek için aşağıdaki afişlerden seçim yapabilirsiniz. Sağlanan kodları kopyalayıp web sitenize yapıştırın ve yayınlayın.',
+  'how-descr-3' => 'Referans bağlantınızla veya eklediğiniz afişle birisi BitDegree web sitenize kaydolduğunda anında BİR token payı alacaksınız.',
+  'how-descr-4' => 'Crowdsale sonunda toplam pay miktarı BDG tokenlerine dönüştürülecek ve referans ettikleri kişi sayısına göre tüm katılımcılar arasında dağıtılacaktır.',
+  'how-descr-5' => 'BitDegree\'ye Telegram & Twitter\'da katıldığınızdan emin olun, bu çok önemlidir! BitDegree tokenlerinizi nasıl alabileceğinizi anlatan yönergeler, kayıt esnasında sağladığınız e-posta adresine gönderilecektir.',
+  'restrictions' => 'Kısıtlamalar',
+  'restriction-1' => 'Birden fazla kez kaydolmaya izin verilmez!',
+  'restriction-2' => 'Kendi kendinizi referans edemezsiniz!',
+  'breaking' => 'Bu kurallardan herhangi birinin ihlali veya herhangi bir suistimal sizin hemen BitDegree ortaklık programından engellenmenize neden olacaktır.',
+  'banners-to-use' => 'Kullanabileceğiniz afişler',
+  'banners-to-use-subtitle' => 'BitDegree afişlerini kullanın ve daha fazla tokeni ücretsiz alın! Basitçe sağlanan kodları web sitenize kopyalayın & yapıştırın ve sitenizde yayınlayın.',
+  '1-referral' => '1 referans',
+  'n-referrals' => ':number referans',
+  'banner' => 'Afiş',
+  'banner-alt' => 'BitDegree:From EA kurucu ortağı, geçmiş COURSERA Başkanı & 29,000,000 kullanıcı. Sınırlı %15 indirim - Token Alın!',
+);
---
+++ resources/lang/tr/auth.php
@@ -0,0 +1,6 @@
+<?php 
+
+return array (
+  'failed' => 'These credentials do not match our records.',
+  'throttle' => 'Çok fazla giriş denemesi. Lütfen :seconds saniye içerisinde tekrar deneyiniz.',
+);
