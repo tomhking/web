4fc2732e09f4f91a931abe824e1feaab4ce5516d
Martynas <martynas.andriuskevicius@hostinger.com>
2017-12-06 17:13:37 +0200

    Add job for updating transaction list in cache

---
+++ app/Console/Commands/UpdateIcoTxns.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Console\Commands;
+
+use GuzzleHttp\Client;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class UpdateIcoTxns extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ico:update-txns';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Retrieves ICO progress and stores the value in cache.';
+
+
+    private $client;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->client = new Client();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $response = $this->client->get('https://api.etherscan.io/api', [
+            'query' => [
+                'module' => 'account',
+                'action' => 'txlist',
+                'address' => env('ICO_ADDRESS'),
+                'startblock' => 0,
+                'endblock' => '999999999999990',
+                'apikey' => env('ETHERSCAN_API_KEY'),
+                'sort' => 'asc',
+            ],
+            'timeout' => 30,
+            'http_errors' => false,
+        ]);
+
+        $content = $response->getBody()->getContents();
+
+        if ($response->getStatusCode() != 200) {
+            $this->line('Could not retrieve the transactions: ' . $content);
+            Log::error('Could not retrieve the transactions', [
+                'status' => $response->getStatusCode(),
+                'body' => $content,
+            ]);
+            return;
+        }
+
+        $response = json_decode($content);
+
+        if(!isset($response->result)){
+            $this->line('Etherscan returned results in an unknown format: '.$content);
+            Log::error('Etherscan returned results in an unknown format', [
+                'body' => $response,
+            ]);
+            return;
+        }
+
+        $txns = collect($response->result)->filter(function($tx) {
+            return $tx->isError == 0 && $tx->value != 0;
+        })->keyBy('hash');
+
+        $holders = $txns->groupBy('from')->sortBy(function ($txns) {
+            foreach($txns as $tx) {
+                $sum = bcadd($sum ?? 0, $tx->value);
+            }
+            return $sum ?? 0;
+        })->reverse();
+
+        $this->line('Transactions and holders updated.');
+
+        cache()->forever('token-holders', $holders);
+        cache()->forever('ico-txns', $txns);
+    }
+}
