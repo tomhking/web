84363679a729ddb2fd71ae290e89bc70cfabe8ee
dovile <dovile.tarutyte@hostinger.com>
2017-12-08 12:41:32 +0200

    Add active sidebar class

--- resources/views/partials/instructions-menu.blade.php
+++ resources/views/partials/instructions-menu.blade.php
@@ -1,11 +1,11 @@
 <div class="col-sm-3 col-md-2 hidden-xs-down sidebar">
     <h4>@lang('ico-instructions.how-to-participate')</h4>
     <ul class="sidebar-nav">
-        <li class="instructions"><a href="{{ route('participate') }}">1. @lang('ico-instructions.article-get-wallet')</a></li>
-        <li class="instructions"><a href="{{ route('participate2') }}">2. @lang('ico-instructions.article-get-eth')</a></li>
-        <li class="instructions"><a href="{{ route('participate4') }}">3. How to get BDG tokens with ETH</a></li>
-        <li class="instructions"><a href="{{ route('participate5') }}">4. How to check my BDG tokens</a></li>
-        <li class="instructions"><a href="{{ route('participate3') }}">@lang('ico-instructions.article-get-tokens')</a></li>
-        <li class="instructions"><a href="{{ route('participatefaq') }}">@lang('ico-instructions.article-faq')</a></li>
+        <li class="{{ current_route_class('participate') }} instructions"><a href="{{ route('participate') }}">1. @lang('ico-instructions.article-get-wallet')</a></li>
+        <li class="{{ current_route_class('participate2') }} instructions"><a href="{{ route('participate2') }}">2. @lang('ico-instructions.article-get-eth')</a></li>
+        <li class="{{ current_route_class('participate4') }} instructions"><a href="{{ route('participate4') }}">3. How to get BDG tokens with ETH</a></li>
+        <li class="{{ current_route_class('participate5') }} instructions"><a href="{{ route('participate5') }}">4. How to check my BDG tokens</a></li>
+        <li class="{{ current_route_class('participate3') }} instructions"><a href="{{ route('participate3') }}">@lang('ico-instructions.article-get-tokens')</a></li>
+        <li class="{{ current_route_class('participatefaq') }} instructions"><a href="{{ route('participatefaq') }}">@lang('ico-instructions.article-faq')</a></li>
     </ul>
 </div>
--- resources/views/partials/referral-menu.blade.php
+++ resources/views/partials/referral-menu.blade.php
@@ -1,9 +1,9 @@
 <div class="col-sm-3 col-md-2 hidden-xs-down sidebar">
     <h4>Referral program</h4>
     <ul class="sidebar-nav">
-        <li class="instructions"><a href="{{ route('affiliate') }}">Referral link</a></li>
-        <li class="instructions"><a href="{{ route('affiliate2') }}" >Banners</a></li>
-        <li class="instructions"><a href="{{ route('affiliate3') }}" >My earnings</a></li>
-        <li class="instructions"><a href="{{ route('affiliate4') }}">How it works?</a></li>
+        <li class="{{ current_route_class('affiliate') }} instructions"><a href="{{ route('affiliate') }}">Referral link</a></li>
+        <li class="{{ current_route_class('affiliate2') }} instructions"><a href="{{ route('affiliate2') }}" >Banners</a></li>
+        <li class="{{ current_route_class('affiliate3') }} instructions"><a href="{{ route('affiliate3') }}" >My earnings</a></li>
+        <li class="{{ current_route_class('affiliate4') }} instructions"><a href="{{ route('affiliate4') }}">How it works?</a></li>
     </ul>
 </div>
