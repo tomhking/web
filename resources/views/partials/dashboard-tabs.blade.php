b32f936068cfdb238b35c3106e676d8ae7291f4b
dovile <dovile.tarutyte@hostinger.com>
2017-12-12 16:55:24 +0200

    nav in progress

--- resources/views/partials/dashboard-tabs.blade.php
+++ resources/views/partials/dashboard-tabs.blade.php
@@ -1,12 +1,37 @@
-<div class="dashboard-navigation">
-    <ul class="nav nav-tabs">
-        @if(config('ico.started'))
-            <li class="{{ current_route_class('address') }}"><a href="{{ route('address') }}"><img class="token" src="{{ asset_rev('token-img.png') }}" alt="BitDegree Token"> @lang('user.tab-crowdsale')</a></li>
-        @endif
-        <li class="{{ current_route_class('participate') }}"><a href="{{ route('participate') }}"><i class="fa fa-question-circle" aria-hidden="true"></i> @lang('user.tab-how')</a></li>
-        <li class="{{ current_route_class('affiliate') }}"><a href="{{ route('affiliate') }}"><i class="fa fa-star" aria-hidden="true"></i> @lang('user.tab-earn')</a></li>
-        @if(auth()->user()->isAirdropParticipant())
-            <li class="{{ current_route_class('user') }}"><a href="{{ route('user') }}"><i class="fa fa-plane" aria-hidden="true"></i> @lang('user.tab-airdrops')</a></li>
-        @endif
-    </ul>
-</div>
+
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container-fluid">
+
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <i class="fa fa-bars" aria-hidden="true"></i>
+            </button>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse dashboard-navigation" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav nav-tabs">
+                @if(config('ico.started'))
+                    <li class="{{ current_route_class('address') }}"><a href="{{ route('address') }}"><img class="token" src="{{ asset_rev('token-img.png') }}" alt="BitDegree Token"> @lang('user.tab-crowdsale')</a></li>
+                @endif
+                <li class="{{ current_route_class('participate') }}"><a href="{{ route('participate') }}"><i class="fa fa-question-circle" aria-hidden="true"></i> @lang('user.tab-how')</a></li>
+
+                <li class="dropdown">
+                    <a href="{{ route('affiliate') }}" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-star" aria-hidden="true"></i> @lang('user.tab-earn') <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li class="{{ current_route_class('affiliate') }} instructions"><a href="{{ route('affiliate') }}">Referral link</a></li>
+                        <li class="{{ current_route_class('affiliate2') }} instructions"><a href="{{ route('affiliate2') }}" >Banners</a></li>
+                        <li class="{{ current_route_class('affiliate3') }} instructions"><a href="{{ route('affiliate3') }}" >My earnings</a></li>
+                        <li class="{{ current_route_class('affiliate4') }} instructions"><a href="{{ route('affiliate4') }}">How it works?</a></li>
+                    </ul>
+                </li>
+                @if(auth()->user()->isAirdropParticipant())
+                    <li class="{{ current_route_class('user') }}"><a href="{{ route('user') }}"><i class="fa fa-plane" aria-hidden="true"></i> @lang('user.tab-airdrops')</a></li>
+                @endif
+                    <!-- .dropdown -->
+            </ul> <!-- .nav .navbar-nav -->
+        </div><!-- /.navbar-collapse -->
+
+    </div><!-- /.container-fluid -->
+</nav>
