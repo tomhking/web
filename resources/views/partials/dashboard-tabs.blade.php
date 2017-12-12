c16d2bb42c3eaa5966c28c540ab7c01015ab4ccd
dovile <dovile.tarutyte@hostinger.com>
2017-12-12 17:29:29 +0200

    fix menu

--- resources/views/partials/dashboard-tabs.blade.php
+++ resources/views/partials/dashboard-tabs.blade.php
@@ -15,8 +15,20 @@
                 @if(config('ico.started'))
                     <li class="{{ current_route_class('address') }}"><a href="{{ route('address') }}"><img class="token" src="{{ asset_rev('token-img.png') }}" alt="BitDegree Token"> @lang('user.tab-crowdsale')</a></li>
                 @endif
-                <li class="{{ current_route_class('participate') }}"><a href="{{ route('participate') }}"><i class="fa fa-question-circle" aria-hidden="true"></i> @lang('user.tab-how')</a></li>
+                <li class="{{ current_route_class('participate') }} hidden"><a href="{{ route('participate') }}"><i class="fa fa-question-circle" aria-hidden="true"></i> @lang('user.tab-how')</a></li>
+                    <li class="dropdown">
+                        <a href="{{ route('participate') }}" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-question-circle" aria-hidden="true"></i> @lang('user.tab-how') <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li class="{{ current_route_class('participate') }} instructions"><a href="{{ route('participate') }}">1. @lang('ico-instructions.article-get-wallet')</a></li>
+                            <li class="{{ current_route_class('participate2') }} instructions"><a href="{{ route('participate2') }}">2. @lang('ico-instructions.article-get-eth')</a></li>
+                            <li class="{{ current_route_class('participate4') }} instructions"><a href="{{ route('participate4') }}">3. How to get BDG tokens with ETH</a></li>
+                            <li class="{{ current_route_class('participate5') }} instructions"><a href="{{ route('participate5') }}">4. How to check my BDG tokens</a></li>
+                            <li class="{{ current_route_class('participate3') }} instructions"><a href="{{ route('participate3') }}">@lang('ico-instructions.article-get-tokens')</a></li>
+                            <li class="{{ current_route_class('participatefaq') }} instructions"><a href="{{ route('participatefaq') }}">@lang('ico-instructions.article-faq')</a></li>
+                        </ul>
+                    </li>
 
+                <li class="{{ current_route_class('affiliate') }} hidden"><a href="{{ route('affiliate') }}"><i class="fa fa-star" aria-hidden="true"></i> @lang('user.tab-earn')</a></li>
                 <li class="dropdown">
                     <a href="{{ route('affiliate') }}" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-star" aria-hidden="true"></i> @lang('user.tab-earn') <span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
