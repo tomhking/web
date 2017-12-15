8a95409db7daa244147c0a7d17cad07939575377
dovile <dovile.tarutyte@hostinger.com>
2017-12-15 13:18:49 +0200

    Add token economy exampe CN

--- resources/views/partials/landing/prototype-economy.blade.php
+++ resources/views/partials/landing/prototype-economy.blade.php
@@ -3,7 +3,13 @@
         <div class="row">
             <div class="col-xs-12">
                 <h2 class="title center">Prototype token economy</h2>
-                <img src="{{ asset_rev('token-economy-example.png') }}" alt="Token economy example">
+
+                @if($currentLanguage == "cn")
+                    <img src="{{ asset_rev('token-economy-example-cn.jpg') }}" alt="Token economy example">
+                @else
+                    <img src="{{ asset_rev('token-economy-example.png') }}" alt="Token economy example">
+                @endif
+
             </div>
         </div>
         <hr style="margin-top:55px; margin-bottom:-55px;">
