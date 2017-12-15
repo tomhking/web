cb99a345c34f41512159e4628ad86d5e94162145
dovile <dovile.tarutyte@hostinger.com>
2017-12-15 09:30:55 +0200

    Add ethos as partner

--- resources/views/partials/landing/partners.blade.php
+++ resources/views/partials/landing/partners.blade.php
@@ -6,57 +6,65 @@
             </div>
         </div>
 
-        <div class="col-xs-12 col-md-8 col-md-offset-2">
+        <div class="col-xs-12 col-md-10 col-md-offset-1">
             <div class="row">
-                <div class="col-xs-4 col-sm-4 text-center">
+                <div class="col-xs-4 col-sm-3 text-center">
+                    <div class="partner-logo">
+                        <a href="https://www.ethos.io/" rel="nofollow" target="_blank"><img src="{{ asset_rev('partners/ethos.png') }}" alt="Our partners Ethos"></a>
+                    </div>
+                </div>
+
+                <div class="col-xs-4 col-sm-3 text-center">
                     <div class="partner-logo">
                         <a href="https://blog.bitdegree.org/press-release-bitdegree-teams-up-with-zcoin-to-develop-cryptography-course-ecfe20f90a56" rel="nofollow" target="_blank"><img src="{{ asset_rev('partners/zcoin.png') }}" alt="Our partners Zcoin"></a>
                     </div>
                 </div>
-                <div class="col-xs-4 col-sm-4 text-center">
+                <div class="col-xs-4 col-sm-3 text-center">
                     <div class="partner-logo">
-                        <a href="https://blog.bitdegree.org/press-release-bitdegree-and-singularitynet-ally-to-develop-artificial-intelligence-courses-975e224272be" rel="nofollow" target="_blank"><img src="{{ asset_rev('partners/singularitynet.png') }}" alt="Our partners SingularityNet"></a>
+                        <a href="https://blog.bitdegree.org/press-release-bitdegree-and-singularitynet-ally-to-develop-artificial-intelligence-courses-975e224272be" rel="nofollow" target="_blank"><img src="{{ asset_rev('partners/singularitynet-2.png') }}" alt="Our partners SingularityNet"></a>
                     </div>
                 </div>
-                <div class="col-xs-4 col-sm-4 text-center">
+                <div class="col-xs-4 col-sm-3 text-center">
                         <div class="partner-logo">
-                             <a href="https://www.tokenlot.com/" rel="nofollow" target="_blank"><img src="{{ asset_rev('partners/tokenlot.png') }}" alt="Our partners Tokenlot"></a>
+                             <a href="https://www.tokenlot.com/" rel="nofollow" target="_blank"><img src="{{ asset_rev('partners/tokenlot-2.png') }}" alt="Our partners Tokenlot"></a>
                         </div>
                 </div>
             </div>
 
             <div class="row">
-                <div class="col-xs-4 col-sm-4 text-center">
+                <div class="col-xs-4 col-sm-3 text-center">
                     <div class="partner-logo">
                         <a href="https://entethalliance.org/" rel="nofollow" target="_blank"><img src="{{ asset_rev('partners/ethereum-alliance-small.png') }}" alt="Our partners Ethereum Alliance"></a>
                     </div>
                 </div>
-                <div class="col-xs-4 col-sm-4 text-center">
+                <div class="col-xs-4 col-sm-3 text-center">
                     <div class="partner-logo">
                         <a href="https://www.hostinger.com/" rel="nofollow" target="_blank"><img src="{{ asset_rev('partners/hostinger.png') }}" alt="Our partners Hostinger"></a>
                     </div>
                 </div>
 
-                <div class="col-xs-4 col-sm-4 text-center">
+                <div class="col-xs-4 col-sm-3 text-center">
+                    <div class="partner-logo">
+                        <a href="https://www.000webhost.com//" rel="nofollow" target="_blank"><img src="{{ asset_rev('partners/000-webhost.png') }}" alt="Our partners 000webhost"></a>
+                    </div>
+                </div>
+
+                <div class="col-xs-4 col-sm-3 text-center">
                     <div class="partner-logo">
                         <a href="https://bitcoinfoundation.org/" rel="nofollow" target="_blank"><img src="{{ asset_rev('partners/bitcoin-foundation-small.png') }}" alt="Our partners Tokenlot"></a>
                     </div>
                 </div>
             </div>
             <div class="row">
-                <div class="col-xs-4 col-sm-4 text-center">
-                    <div class="partner-logo">
-                        <a href="https://www.000webhost.com//" rel="nofollow" target="_blank"><img src="{{ asset_rev('partners/000-webhost.png') }}" alt="Our partners 000webhost"></a>
-                    </div>
-                </div>
 
-                <div class="col-xs-4 col-sm-4 text-center">
+
+                <div class="col-xs-4 col-sm-3 col-sm-offset-3 text-center">
                     <div class="partner-logo">
                         <a href="https://nexchange.io/" rel="nofollow" target="_blank"><img src="{{ asset_rev('partners/nexchange-small.png') }}" alt="Our partners Nexchange"></a>
                     </div>
                 </div>
 
-                <div class="col-xs-4 col-sm-4 text-center">
+                <div class="col-xs-4 col-sm-3 text-center">
                     <div class="partner-logo">
                         <a href="https://preply.com" rel="nofollow" target="_blank"><img src="{{ asset_rev('partners/preply.png') }}" alt="Our partners Preply"></a>
                     </div>
