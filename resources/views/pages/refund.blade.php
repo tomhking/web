686383a85e085fd6f64f48d574e952a9c69de408
dovile <dovile.tarutyte@hostinger.com>
2017-12-29 15:25:35 +0200

    Update refund texts

--- resources/views/pages/refund.blade.php
+++ resources/views/pages/refund.blade.php
@@ -17,19 +17,21 @@
                     <div class="row">
                         <div class="col-xs-12 col-md-12">
 
-                            <p>BDG token — is a utility token that will be used in the operation cycles of the learning platform and will represent future access to a platform’s products and services.</p>
+                            <p>BDG token — is a utility token that will represent the future access to a learning platform’s services. BitDegree is fully compliant with law and wants to focus on project only, without taking any risks or putting contributors into it, which can affect the success of the whole blockchain based learning platform. It has <a style="color: #ffbcbc; font-weight:bold;" href="https://blog.bitdegree.org/bank-of-lithuania-confirms-bitdegree-token-and-model-of-operation-is-fully-within-legal-frameworks-ad0cb8e335c2">received confirmation</a> from Bank of Lithuania, acting under maintenance of European Central Bank, that token and model of operation is fully within legal frameworks.</p>
 
-                            <p><a style="color: #ffbcbc; font-weight:bold;" href="https://blog.bitdegree.org/bank-of-lithuania-confirms-bitdegree-token-and-model-of-operation-is-fully-within-legal-frameworks-ad0cb8e335c2">BitDegree is fully compliant with law</a> and wants to focus on project only, without taking any risks or putting contributors into it, which can affect the success of the whole blockchain based learning platform. It has received confirmation from Bank of Lithuania, acting under maintenance of European Central Bank, that token and model of operation is fully within legal frameworks.</p>
+                            <p>As you may already know, taking into account newest legal practice in United States regarding securities & non-securities starting from middle of December 2017, questions related with tokens and causing uncertainty, BitDegree voluntarily decided to avoid any possible legal risks, including minimal or hypothetical, and to stop new contributions from United States residents, citizens, green card holders and taxpayers at BitDegree’s crowdsale.</p>
 
-                            <p>Taking into account latest legal practice in United States regarding securities & non-securities starting from middle of December 2017, questions related with tokens and causing uncertainty, BitDegree voluntarily decided to avoid any possible legal risks, including minimal or hypothetical, and to stop NEW contributions from United States residents, citizens, green card holders and taxpayers at BitDegree’s crowdsale.</p>
+                            <p>According to above, residents of the United States participated in Bitdegree’s crowdsale will have an ability to be voluntarily refunded until 31th January, 2018, if they wish so.</p>
 
-                            <p>For existing US contributors, BitDegree will provide the full possibility to continue with the BDG tokens and the platform. If after our e-mail you feel that you no longer want to participate in the future development of the platform as a contributor, you can choose to file for a refund after the crowdsale until 31th January, 2018. This is our goodwill and your voluntary choice.</p>
+                            <p>Before taking decision regarding your possibility to refund please kindly be informed, that:</p>
 
-                            <p>US law (enforced by the SEC) with respect to ICOs is evolving and we are trying to mitigate any potential risk by offering an optional refund to US citizens or residents.</p>
+                            <p><b>(i) BitDegree token is a utility token that will be used in the operation cycles of the learning platform and will represent future access to a platform’s products and services;</b></p>
 
-                            <p><b>This is completely optional and you can see it as a proactive move on our part.</b></p>
+                            <p><b>(ii) there is no real assumptions or probabilities to believe or assume, that BitDegree token’s value will rise or grow. So, you should not have any though, guesses or forecasts, that token’s value may or will grow or rise;</b></p>
 
-                            <p><b>We’re building a blockchain-based learning platform as it was planned and the crowdsale continues getting it’s highs with no stops nor barriers!</b></p>
+                            <p><b>(iii) purchase of BitDegree tokens shall be based just on participation on the project, not on speculative or investments motives. Therefore, if you purchased BitDegree tokens in order receive any profit from that later, please use our refund program, because, as mentioned above, there is no assumptions to believe, that BitDegree token’s value will or may rise.</b></p>
+
+                            <p>In case you decided to be refunded, you may follow below mentioned instructions: Starting from January 1, 2018, login into your BitDegree dashboard, go to refund section and follow provided instructions.</p>
 
                             <p>We believe that BitDegree Learning Platform will succeed and revolutionize education from the base.</p>
 
